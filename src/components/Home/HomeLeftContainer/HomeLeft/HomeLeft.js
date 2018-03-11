3c2787fe9d85e179980420a734b8ac8fdc5fc1cb
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2018-03-11 16:49:09 +0700

    :fire: Layout the app

--- src/components/Home/HomeLeftContainer/HomeLeft/HomeLeft.js
+++ src/components/Home/HomeLeftContainer/HomeLeft/HomeLeft.js
@@ -8,14 +8,59 @@ const HomeLeft = ({ userProfile }) => {
         <div className='description'>
           <h4> {userProfile.firstName} {userProfile.lastName}</h4>
           <h5>
-            <strong> Website Designer </strong>
+            <strong> Full-stack JavaScript Developer </strong>
           </h5>
-          <p>
-            Pellentesque elementum dapibus convallis.
-                            </p>
+          <p>Quote: {userProfile.profile.quote}</p>
+          <p>Studied at: {userProfile.profile.studiedAt}</p>
+          <p>Lives in: {userProfile.profile.livesIn}</p>
+          <p>Marital status: {userProfile.profile.maritalStatus}</p>
+          <p>Followed by: {userProfile.profile.followedBy}</p>
+        </div>
+      </div>
+
+      <br />
+
+      <div className='user-wrapper'>
+        <div className='description'>
+          <h3> Social Links: </h3>
           <hr />
-          <a href='#' className='btn btn-danger btn-sm'>
-            <i className='fa fa-user-plus' /> &nbsp;Profile + </a>
+          <a href='{userProfile.profile.website}' className='btn btn-sm btn-social btn-facebook' target='_blank'>
+            <i className='fa fa-chrome' />&nbsp; Website </a>
+          <a href='{userProfile.profile.github}' className='btn btn-sm btn-social btn-google' target='_blank'>
+            <i className='fa fa-github' />&nbsp; GitHub</a>
+          <a href='{userProfile.profile.twitter}' className='btn btn-sm btn-social btn-twitter' target='_blank'>
+            <i className='fa fa-twitter' />&nbsp; Twitter </a>
+          <a href='{userProfile.profile.linkedIn}' className='btn btn-sm btn-social btn-linkedin' target='_blank'>
+            <i className='fa fa-linkedin' />&nbsp; Linkedin </a>
+        </div>
+      </div>
+
+      <br />
+
+      <div className='user-wrapper'>
+        <div className='description'>
+          <h3> Friends: </h3>
+          <hr />
+          <ul className='list-group'>
+            <li className='list-group-item'>
+              <div className='col-sm-6'>
+                <img src='http://api.randomuser.me/portraits/men/49.jpg' alt='Scott Stevens' className='img-responsive img-circle' />
+              </div>
+              <div className='col-sm-6'>
+                <img src='http://api.randomuser.me/portraits/men/49.jpg' alt='Scott Stevens' className='img-responsive img-circle' />
+              </div>
+              <div className='clearfix' />
+            </li>
+            <li className='list-group-item'>
+              <div className='col-sm-6'>
+                <img src='http://api.randomuser.me/portraits/men/49.jpg' alt='Scott Stevens' className='img-responsive img-circle' />
+              </div>
+              <div className='col-sm-6'>
+                <img src='http://api.randomuser.me/portraits/men/49.jpg' alt='Scott Stevens' className='img-responsive img-circle' />
+              </div>
+              <div className='clearfix' />
+            </li>
+          </ul>
         </div>
       </div>
     </div>
--- src/components/Home/HomeRightContainer/HomeRight/HomeRight.js
+++ src/components/Home/HomeRightContainer/HomeRight/HomeRight.js
@@ -2,31 +2,52 @@ import React from 'react'
 
 const HomeRight = () => {
   return (
-    <div className='col-md-9 col-sm-9  user-wrapper'>
-      <div className='description'>
-        <h3> Ramolin's Biography : </h3>
-        <hr />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit egestas eros, ut maximus magna blandit ut. Suspendisse
+    <div className='col-md-9 col-sm-9'>
+
+      <div className='user-wrapper'>
+        <div className='description'>
+          <h3> Make Post </h3>
+          <hr />
+          <form>
+            <div className='form-group'>
+              <label for='comment'>What's on your mind?</label>
+              <textarea className='form-control' rows='5' id='comment' />
+            </div>
+          </form>
+          <button type='button' className='btn btn-primary'>Post</button>
+        </div>
+      </div>
+
+      <br />
+
+      <div className='user-wrapper'>
+        <div className='description'>
+          <h3> Xinh Nguyen - Feb/12/2018 </h3>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit egestas eros, ut maximus magna blandit ut. Suspendisse
             vel molestie sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce sit amet
             egestas dui, vitae tincidunt justo.
         </p>
-        <p>
-          Etiam eleifend massa congue magna pretium, id blandit dui pulvinar. Nam et pulvinar risus, vel blandit risus. Maecenas tristique
-            volutpat ante, aliquet hendrerit felis aliquet nec. Curabitur rutrum tincidunt erat, ac feugiat libero
-            gravida ac. Suspendisse ut varius quam, vitae rutrum metus.
-        </p>
-        <h3> Social Links: </h3>
-        <hr />
-        <a href='#' className='btn btn-social btn-facebook'>
-          <i className='fa fa-facebook' />&nbsp; Facebook </a>
+          <button type='button' className='btn btn-primary'>Like</button>
+          <button type='button' className='btn btn-default'>Comment</button>
+        </div>
+      </div>
 
-        <a href='#' className='btn btn-social btn-google'>
-          <i className='fa fa-google-plus' />&nbsp; Google</a>
-        <a href='#' className='btn btn-social btn-twitter'>
-          <i className='fa fa-twitter' />&nbsp; Twitter </a>
-        <a href='#' className='btn btn-social btn-linkedin'>
-          <i className='fa fa-linkedin' />&nbsp; Linkedin </a>
+      <br />
+
+      <div className='user-wrapper'>
+        <div className='description'>
+          <h3> Xinh Nguyen - Feb/10/2018 </h3>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit egestas eros, ut maximus magna blandit ut. Suspendisse
+            vel molestie sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce sit amet
+            egestas dui, vitae tincidunt justo.
+        </p>
+          <button type='button' className='btn btn-primary'>Like</button>
+          <button type='button' className='btn btn-default'>Comment</button>
+        </div>
       </div>
 
     </div>
