a29bafbaa8d5732a8dc5b753e6cfce108ba669ce
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2018-03-14 08:08:37 +0700

    :sparkles: Clean architecture

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,2 +1,2 @@
-export const HOST_API = 'https://younet.herokuapp.com'
+export const HOST_API = 'https://younet-api.herokuapp.com'
 export const GET_USER_PROFILE = '/api/get-user-profile-by-username/'
